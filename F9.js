0339301c76d4569b9b5c59cbed390a8a1bc9caed
Joseph-J-Friday <83015160+Joseph-J-Friday@users.noreply.github.com>
2021-10-05 10:18:27 +0100

    Add files via upload

--- F9.js
+++ F9.js
@@ -1,10 +1,15 @@
 "use strict";
 let obj = function(params){
 	params = params || {};
-	this.space = space || document.body;
+	this.space = params.space || document.body;
 	this.main = params.main !== undefined ? params.main : false;
+	this.main === true ? MAIN = this : null;
 	this.x = params.x || 0;
+	this.jcount = 0;
+	this.maxH = params.maxH || 25;
 	this.y = params.y|| 0;
+	this.fixedX = this.x;
+	this.fixedY = this.y;
 	this.w = sizeW(params.w || 8);
 	this.h = sizeH(params.h || 8);
 	this.mass = params.mass || 0;
@@ -17,6 +22,7 @@ let obj = function(params){
 	this.forceReserve = this.force;
 	this.stroke = params.stroke || 0;
 	this.speed = params.speed || calcSpeed(this.mass);
+	this.speedReserve = this.speed;
 	this.onMe = this;
 	this.floor = this;
 	this.id = Math.random()*100;
@@ -40,12 +46,13 @@ let obj = function(params){
 	this.ws.textAlign = "center";
 	this.ws.fontWeight = "bolder";
 	this.ws.overflow = "hidden";
+	this.ws.display = "block";
 	this.roundness = params.roundness || 0;
 	this.ws.borderRadius = this.roundness + "%";
 	if(this.texture !== undefined){
 		this.ws.backgroundImage = "URL(" + this.texture + ")";
-		this.ws.backgroundOrigin = "top"
-		this.ws.backgroundSize = "cover"
+		this.ws.backgroundOrigin = "left";
+		this.ws.backgroundSize = "cover";
 	}else{this.ws.backgroundColor = this.hue;}
 	this.ws.border = this.stroke + "px solid white"
 	this.w += sizeW(this.stroke*2);
@@ -64,13 +71,39 @@ obj.prototype.setRoundness = function(value){
 	this.roundness = value;
 	this.ws.borderRadius = this.roundness + "%";
 }
-obj.prototype.halt = function(AF){
+obj.prototype.halt = function(AF,speed=0){
 	AF === 0 ? cancelAnimationFrame(this.mlA) : cancelAnimationFrame(this.mrA);
+	UnDoo(speed,AF)
 	preLoop();
 }
 obj.prototype.jump = function(){
 	if(this.landed === true){
-		
+		this.y -= 1;
+		//this.onMe.jump();
+		for(let i = 0; i<Bodies.length; i++){
+			if(this!==Bodies[i]){
+				let b1 = this.y+this.h;
+				let b2 = Bodies[i].y+Bodies[i].h;
+				if((this.y<=b2) && (b1>b2) && (this.x<=(Bodies[i].x+Bodies[i].w)) && ((this.x+this.w)>=Bodies[i].x)){
+					if(Bodies[i].mass !== 0 && Bodies[i].mass<this.mass){Bodies[i].force = this.force+.001;}
+					if(Bodies[i].mass === 0){this.jcount = this.maxH}
+					if(Bodies[i].mass>this.mass){this.jcount = this.maxH}
+					//Bodies[i].onMe = this;
+					this.force = this.forceReserve;
+					preLoop()
+				}
+			}
+		}
+		this.ws.top = this.y + "%";
+		if(this.jcount<this.maxH){
+		this.jcount++;
+		this.jAnim = requestAnimationFrame(()=>{this.jump()});
+		}else if(this.jcount>=this.maxH){
+		this.jcount = 0;
+		this.landed = false;
+		cancelAnimationFrame(this.jAnim);
+		return;
+		}
 	}
 }
 obj.prototype.fall = function(){
@@ -105,28 +138,39 @@ obj.prototype.fall = function(){
 	else{return}
 }
 
-obj.prototype.right = function(step=false,v,pusher){
+obj.prototype.right = function(step=false,v,pusher=this){
 	if(this.dir === 0){
 		preLoop();
 		this.dir = 1;
 	}
 	else{this.dir = 1}
+	undoList = [];
 	let p = pusher;
 	!step ? this.pushingMe = this : null;
 	this.collided = this;
 	this.speed = v || this.speed;
 	if(this.main === false){
-		(this.x+this.w<100) ? this.x += this.speed : null;
+		if(this.x+this.w<xend){this.x += this.speed; this.fixedX += this.speed}
 	}
 	else{
 		if(this.x<50 || xlim>=WIDTH){
 			this.midway = false;
-			(this.x+this.w<100) ? this.x += this.speed : null;
+			if(this.x+this.w<WIDTH){this.x += this.speed; this.fixedX += this.speed}
+		}
+		if(xlim>=WIDTH && WIDTH>=200){
+			for(let i = 0; i<Bodies.length; i++){
+				if(Bodies[i]!==MAIN){
+					Bodies[i].x = Bodies[i].fixedX-(WIDTH-100);
+					Bodies[i].ws.left = Bodies[i].x + "%";
+				}
+			}
+			xlim = WIDTH;
 		}
 		if(this.x>=50 && xlim<WIDTH){
 			this.midway = true;
-			console.log(xlim)
-			xbegin -= this.speed;
+			this.x = this.fixedX = 50;
+			this.ws.left = this.x + "%";
+			xbegin,xend -= this.speed;
 			xlim += this.speed;
 		}
 	}	
@@ -137,9 +181,11 @@ obj.prototype.right = function(step=false,v,pusher){
 		this.base = HEIGHT;
 		this.floor.onMe = this.floor;
 	}
-	if((this.x+this.w)>=100){
-		this.x = (100-this.w);
+	if((this.x+this.w)>=xend){
+		this.x = (xend-this.w);
+		this.ws.left = this.x + "%";
 		this.pushingMe !== this ? this.pushingMe.alignR(this.x-this.pushingMe.w) : null;
+		return;
 	}
 	if(this.x>(this.onMe.x+this.onMe.w)){
 		this.onMe.landed = false;
@@ -150,6 +196,7 @@ obj.prototype.right = function(step=false,v,pusher){
 		if(this.main === true && this.x >= 50 && xlim<WIDTH && Bodies[i] !== this){
 			Bodies[i].x -= this.speed;
 			Bodies[i].ws.left = Bodies[i].x + "%";
+			undoList.push(Bodies[i]);	
 		}
 		if(this.collided === this && this.id!==Bodies[i].id && Bodies[i].id!==this.floor.id){
 			if(((this.x+this.w)>=Bodies[i].x) && ((this.x+this.w)<(Bodies[i].x+Bodies[i].w))){
@@ -159,7 +206,7 @@ obj.prototype.right = function(step=false,v,pusher){
 						if(!step){
 							p = this;
 							this.prevCollided !== Bodies[i] ? p.massScale += Bodies[i].mass : null;
-							console.log(p.massScale + " 1st " + this.mass)
+						//	console.log(p.massScale + " 1st " + this.mass)
 						}
 						else{
 							this.prevCollided !== Bodies[i] ? p.massScale += Bodies[i].mass : null;
@@ -171,14 +218,41 @@ obj.prototype.right = function(step=false,v,pusher){
 							this.collided.right(true,this.speed,p);
 						}
 						else if(this.collided !== this && p.massScale>=p.mass){
+							UnDoo(this.speed,1);
 							this.pushingMe !== this ? this.alignR(this.collided.x-this.w) : null;
 							return;
 						}
 					//stop	
 					}
 					else{
+						if(this.main && !this.midway){
+							UnDoo(this.speed,1);
+							this.x = (Bodies[i].x-this.w);
+							this.pushingMe !== this ? this.pushingMe.alignR(this.x-this.pushingMe.w) : null;
+						}	
+						else if(step && pusher === MAIN && pusher.midway && !this.main){
+					//	UnDoo(this.speed,1);
 						this.x = (Bodies[i].x-this.w);
+						this.ws.left = this.x + "%";
+						pusher.halt(1,this.speed);
 						this.pushingMe !== this ? this.pushingMe.alignR(this.x-this.pushingMe.w) : null;
+						
+						console.log("K");
+						}
+						else if(!this.main && !this.midway){
+							UnDoo(this.speed,1);
+						//	this.ws.backgroundColor = "skyblue";
+							this.x = (Bodies[i].x-this.w);
+							this.ws.left = this.x + "%";
+							this.pushingMe !== this ? this.pushingMe.alignR(this.x-this.pushingMe.w) : null;
+							
+						}
+						else{
+							UnDoo(this.speed,1);
+							this.x = thie.fixedX = (Bodies[i].x-this.w);
+							this.ws.left = this.x + "%";
+							return
+						}
 					}
 				}
 				else{}
@@ -190,31 +264,38 @@ obj.prototype.right = function(step=false,v,pusher){
 	this.ws.left = this.x + "%";
 	step ? null : this.mrA = requestAnimationFrame(()=>{this.right()});
 }
-
-obj.prototype.left = function(step=false,v,pusher){
+obj.prototype.left = function(step=false,v,pusher=this){
 	if(this.dir === 1){
 		preLoop();
 		this.dir = 0;
 	}else{this.dir = 0}
+	undoList = [];
 	let p = pusher;
 	!step ? this.pushingMe = this : null;
 	this.collided = this;
 	this.speed = v || this.speed;
 	if(this.main === false){
-		this.x>xbegin ? this.x -= this.speed : null;
+		if(this.x>xbegin){this.x -= this.speed; this.fixedX -= this.speed}
 	}
 	else{
 		if(this.midway === true && xlim<=100){
 			this.midway = false;
-			this.x>xbegin ? this.x -= this.speed : null;
+			for(let i = 0; i<Bodies.length; i++){
+				if(Bodies[i]!==MAIN){
+					Bodies[i].x = Bodies[i].fixedX;
+					Bodies[i].ws.left = Bodies[i].x + "%";
+				}
+			}
+			if(this.x>xbegin){this.x -= this.speed; this.fixedX -= this.speed}
 		}
 		if(this.x>50 && xlim>=WIDTH){
-			this.x>xbegin ? this.x -= this.speed : null;
+			if(this.x>xbegin){this.x -= this.speed; this.fixedX -= this.speed}
 		}
-		else{this.midway = true}
+		else{
+			this.midway = true;
+			}
 		if(this.midway === true && xlim>100){
-			console.log(xlim)
-			xbegin += this.speed;
+			xbegin,xend += this.speed;
 			xlim -= this.speed;
 		}
 	}
@@ -227,7 +308,9 @@ obj.prototype.left = function(step=false,v,pusher){
 	}
 	if(this.x<=xbegin){
 		this.x = xbegin;
+		this.ws.left = this.x + "%";
 		this.pushingMe !== this ? this.pushingMe.alignL(this.x+this.w) : null;
+		return;
 	}
 	if((this.x+this.w)<this.onMe.x){
 		this.onMe.landed = false;
@@ -238,6 +321,7 @@ obj.prototype.left = function(step=false,v,pusher){
 		if(this.main === true && this.midway === true && xlim>100 && Bodies[i] !== this){
 			Bodies[i].x += this.speed;
 			Bodies[i].ws.left = Bodies[i].x + "%";
+			undoList.push(Bodies[i]);
 		}
 		if(this.id!==Bodies[i].id && Bodies[i].id!==this.floor.id){
 			if((this.x<=(Bodies[i].x+Bodies[i].w)) && ((this.x)>Bodies[i].x)){
@@ -247,7 +331,6 @@ obj.prototype.left = function(step=false,v,pusher){
 						if(!step){
 							p = this;
 							this.prevCollided !== Bodies[i] ? p.massScale += Bodies[i].mass : null;
-							console.log(p.massScale + " 1st " + this.mass)
 						}
 						else{
 							this.prevCollided !== Bodies[i] ? p.massScale += Bodies[i].mass : null;	
@@ -259,14 +342,28 @@ obj.prototype.left = function(step=false,v,pusher){
 							this.collided.left(true,this.speed,p);
 						}
 						else if(this.collided !== this && p.massScale>=p.mass){
+							UnDoo(this.speed,0);
 							this.pushingMe !== this ? this.alignL(this.collided.x+this.collided.w) : null;
 							return;
 						}
 					//stop	
 					}
 					else{
-						this.x = (Bodies[i].x+Bodies[i].w);
-						this.pushingMe !== this ? this.pushingMe.alignL(this.x+this.w) : null;
+					if(this.main && !this.midway){
+					this.x = (Bodies[i].x+Bodies[i].w);
+					this.pushingMe !== this ? this.pushingMe.alignL(this.x+this.w) : null;
+					}	
+					else if(!this.main && !this.midway){
+					UnDoo(this.speed,0);
+				//	this.ws.backgroundColor = "skyblue";
+					this.x = (Bodies[i].x+Bodies[i].w);
+					this.pushingMe !== this ? this.pushingMe.alignL(this.x+this.w) : null;
+					
+					}
+					else{
+					UnDoo(this.speed,0);
+					return
+					}
 					}
 				}
 				else{}
@@ -279,28 +376,30 @@ obj.prototype.left = function(step=false,v,pusher){
 	step ? null : this.mlA = requestAnimationFrame(()=>{this.left()});
 }
 obj.prototype.alignL = function(x){
-	this.x = x;
+	this.x = this.fixed = x;
 	this.ws.left = this.x + "%";
 	this.pushingMe !== this ? this.pushingMe.alignL(this.x+this.w) : null;
 }
 obj.prototype.alignR = function(x){
-	this.x = x;
+	this.x = this.fixedX = x;
 	this.ws.left = this.x + "%";
 	this.pushingMe !== this ? this.pushingMe.alignR(this.x-this.pushingMe.w) : null;
 }
 let F9 = function(){
-	window.g = 9.82;
+	window.g = 10;
 	window.Bodies = [];
+	window.undoList = [];
+	window.MAIN = undefined;
 	window.ranHue = randomColor();
-	window.WIDTH = window.xlim = 100;
-	window.HEIGHT = window.ylim = 100;
+	window.WIDTH = window.xend = window.xlim = 100;
+	window.HEIGHT = window.yend = window.ylim = 100;
 	window.xbegin = window.ybegin = 0;
 	window.showmass = false;
 	pull();
 }
 F9.prototype.mapSize = function(w,h){
-	WIDTH = w;
-	HEIGHT = h;
+	WIDTH = xend = w;
+	HEIGHT = yend = h;
 }
 F9.prototype.showMass = function(req=true){
 	if(req){
@@ -332,12 +431,36 @@ const pull = () =>{
 	}
 	requestAnimationFrame(pull);
 }
+const UnDoo = (v,which) =>{
+	if(which === 0){
+		if(undoList.length>0){
+			xbegin,xend -= v;
+			xlim += v
+		}
+		for(let i = 0; i<undoList.length; i++){
+			undoList[i].x -= v;
+			undoList[i].ws.left = undoList[i].x + "%";
+			
+		}
+	}
+	else{
+		if(undoList.length>0){
+			xbegin,xend += v;
+			xlim -= v;
+		}
+		for(let i = 0; i<undoList.length; i++){
+			undoList[i].x += v;
+			undoList[i].ws.left = undoList[i].x + "%";
+			
+		}
+	}
+}
 const preLoop = () =>{
 		for(let i = 0; i<Bodies.length; i++){
 			if(Bodies[i].mass > 0){
 				Bodies[i].prevCollided = Bodies[i];
 				Bodies[i].massScale = 0;
-				Bodies[i].speed = calcSpeed(Bodies[i].mass);
+				Bodies[i].speed = Bodies[i].speedReserve;
 			}
 		}
 }
